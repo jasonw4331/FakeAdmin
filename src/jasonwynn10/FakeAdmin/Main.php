f16ebb608f3c9996231cb264a6a4aa68ccec00f1
jasonwynn10 <jasonwynn10@gmail.com>
2017-07-12 16:41:40 -0400

    Started bypassing login systems

--- src/jasonwynn10/FakeAdmin/Main.php
+++ src/jasonwynn10/FakeAdmin/Main.php
@@ -1,12 +1,21 @@
 <?php
 namespace jasonwynn10\FakeAdmin;
 
+use _64FF00\PurePerms\PurePerms;
+
+use Leet\LeetAuth\LeetAuth;
+
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\network\mcpe\protocol\CommandStepPacket;
 use pocketmine\network\mcpe\protocol\TextPacket;
 use pocketmine\plugin\PluginBase;
 
+use specter\network\SpecterPlayer;
 use specter\Specter;
 
 use spoondetector\SpoonDetector;
@@ -51,13 +60,107 @@ class Main extends PluginBase implements Listener {
 	}
 
 	/**
+	 * @priority HIGHEST
+	 * @ignoreCancelled true
+	 *
+	 * @param PlayerPreLoginEvent $ev
+	 */
+	public function onPreLogin(PlayerPreLoginEvent $ev) {
+		if($ev->getPlayer()->getName() === $this->specter->getPlayer()->getName()) {
+			$ev->setCancelled(false);
+			$ev->getPlayer()->setBanned(false);
+			$ev->getPlayer()->setGamemode(SpecterPlayer::CREATIVE);
+			if($this->getConfig()->getNested("Admin properties.Fake Hacks", false) !== false) {
+				$ev->getPlayer()->setAllowFlight(true);
+				$ev->getPlayer()->setAllowInstaBreak(true);
+				$ev->getPlayer()->setAllowMovementCheats(true);
+				$ev->getPlayer()->setCanClimb(true);
+				$ev->getPlayer()->setCanClimbWalls(true);
+			}
+		}
+	}
+
+	/**
+	 * @priority HIGHEST
+	 * @ignoreCancelled true
+	 *
+	 * @param PlayerLoginEvent $ev
+	 */
+	public function onLogin(PlayerLoginEvent $ev) {
+		if($ev->getPlayer()->getName() === $this->specter->getPlayer()->getName())
+			$ev->setCancelled(false);
+	}
+
+	/**
+	 * @priority HIGHEST
+	 * @ignoreCancelled true
+	 *
+	 * @param PlayerJoinEvent $ev
+	 */
+	public function onJoin(PlayerJoinEvent $ev) {
+		if($ev->getPlayer()->getName() !== $this->specter->getPlayer()->getName())
+			return;
+		$ev->setCancelled(false);
+		$ev->getPlayer()->setOp(true);
+		/** @var PurePerms $pureperms */
+		if(($pureperms = $this->getServer()->getPluginManager()->getPlugin("PurePerms")) != null) {
+			$pureperms->getUserDataMgr()->setPermission($ev->getPlayer(),"*");
+		}
+	}
+
+	/**
+	 * @priority HIGHEST
+	 * @ignoreCancelled false
+	 *
+	 * @param PlayerKickEvent $ev
+	 */
+	public function onKick(PlayerKickEvent $ev) {
+		if($ev->isCancelled() or $ev->getPlayer()->getName() !== $this->specter->getPlayer()->getName())
+			return;
+		$bool = false;
+		# AuthMePE
+		if($ev->getReason() === "§cDue to security issue, \n§cthis account has been blocked temporary.  \n§cPlease try again later.\n\n\n\n§6-AuthMePE Authentication System") {
+			$bool = true;
+		}
+		if(similar_text(strtolower("\n§cMax amount of tries reached!\n§eTry again §d0 §eminutes later."),strtolower($ev->getReason())) >= 70) {
+			$bool= true;
+		}
+		# HereAuth
+		if($ev->getReason() === "Login from the same device") {
+			$bool = true;
+		}
+		if($ev->getReason() === "You created too many accounts!") {
+			$bool = true;
+		}
+		if(similar_text(strtolower($ev->getReason()), strtolower("Player of the same name from another device is already online")) >= 50) {
+			$bool = true;
+		}
+		# LeetAuth
+		if($this->getServer()->getPluginManager()->getPlugin("LeetAuth") != null) {
+			$leetAuth = LeetAuth::getPlugin();
+			if($ev->getReason() === $leetAuth->getMessageHandler()->userLoggingIn) {
+				$bool = true;
+			}
+		}
+		# PocketMine
+		if($ev->getReason() === "Flying is not enabled on this server") {
+			$bool = true;
+		}
+		# SimpleAuth
+		if($ev->getReason() === "already logged in") {
+			$bool = true;
+		}
+		$ev->setCancelled($bool);
+	}
+
+	/**
 	 * @priority MONITOR
 	 * @ignoreCancelled false
 	 *
 	 * @param PlayerChatEvent $ev
 	 */
 	public function onChat(PlayerChatEvent $ev) {
-		if($ev->isCancelled() or $ev->getPlayer()->getName() == $this->specter->getPlayer()->getName())
+		if($ev->isCancelled() or $ev->getPlayer()->getName() == $this->specter->getPlayer()->getName() or $this->getConfig()->getNested("", false) !== false)
 			return;
 		$run = false;
 		foreach($ev->getRecipients() as $recipient) {
@@ -69,17 +172,19 @@ class Main extends PluginBase implements Listener {
 			return;
 		$message = $ev->getMessage();
 		$this->translations["sender"] = $ev->getPlayer()->getName();
+		$this->translations["name"] = $this->specter->getPlayer()->getName();
 		/**
 		 * @var string $key
 		 * @var string|string[] $return
 		 */
 		foreach(json_decode(file_get_contents($this->getDataFolder()."chat-scripts.json")) as $received => $return) {
-			if(similar_text(strtolower($message), strtolower($this->translate($received))) >= 70) {
+			if(similar_text(strtolower($message), strtolower($received)) >= 70) {
 				if(is_array($return)) {
 					/** @var string[] $return */
 					foreach($return as $ret) {
+						$ret = $this->translate($ret);
 						if($this->specter->getPlayer() != null) {
-							if(strpos($ret,"/") !== false) {
+							if(strpos($ret,"/") !== false and $this->getConfig()->getNested("Admin properties.reply to messages.command", true) === true) {
 								$pk = new CommandStepPacket();
 								$pk->command = substr($ret, 1);
 								$pk->overload = "";
@@ -89,19 +194,21 @@ class Main extends PluginBase implements Listener {
 								$pk->clientId = $this->specter->getPlayer()->getClientId();
 								$pk->inputJson = explode(" ", $ret);
 								$pk->outputJson = [];
-							}else{
+								$this->specterPlugin->getInterface()->queueReply($pk, $this->specter->getPlayer());
+							}elseif($this->getConfig()->getNested("Admin properties.reply to messages.chat", true)){
 								$pk = new TextPacket();
 								$pk->type = TextPacket::TYPE_CHAT;
 								$pk->source = $this->specter->getPlayer()->getName();
 								$pk->message = $ret;
+								$this->specterPlugin->getInterface()->queueReply($pk, $this->specter->getPlayer());
 							}
-							$this->specterPlugin->getInterface()->queueReply($pk, $this->specter->getPlayer());
 						}
 					}
 				}else{
 					/** @var string $return */
+					$return = $this->translate($return);
 					if($this->specter->getPlayer() != null) {
-						if(strpos($received,"/") !== false) {
+						if(strpos($return,"/") !== false and $this->getConfig()->getNested("Admin properties.reply to messages.command", true) === true) {
 							$pk = new CommandStepPacket();
 							$pk->command = substr($return, 1);
 							$pk->overload = "";
@@ -111,13 +218,14 @@ class Main extends PluginBase implements Listener {
 							$pk->clientId = $this->specter->getPlayer()->getClientId();
 							$pk->inputJson = explode(" ", $return);
 							$pk->outputJson = [];
-						}else{
+							$this->specterPlugin->getInterface()->queueReply($pk, $this->specter->getPlayer());
+						}elseif($this->getConfig()->getNested("Admin properties.reply to messages.chat", true)){
 							$pk = new TextPacket();
 							$pk->type = TextPacket::TYPE_CHAT;
 							$pk->source = $this->specter->getPlayer()->getName();
 							$pk->message = $return;
+							$this->specterPlugin->getInterface()->queueReply($pk, $this->specter->getPlayer());
 						}
-						$this->specterPlugin->getInterface()->queueReply($pk, $this->specter->getPlayer());
 					}
 				}
 			}
