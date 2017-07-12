09b254a074274d3d1721c30d49ce4e4374814fe8
jasonwynn10 <jasonwynn10@gmail.com>
2017-07-12 08:36:29 -0400

    Plugin stuff!

---
+++ src/jasonwynn10/FakeAdmin/AdminEntity.php
@@ -0,0 +1,12 @@
+<?php
+namespace jasonwynn10\FakeAdmin;
+
+use pocketmine\Server;
+use specter\api\DummyPlayer;
+
+class AdminEntity extends DummyPlayer {
+    public function __construct($name, $address = null, $port = null, Server $server = null){
+        parent::__construct($name, $address, $port, $server);
+        //
+    }
+}
