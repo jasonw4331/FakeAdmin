2876788a184646b7805fad0b9c08fd9940260839
jasonwynn10 <jasonwynn10@gmail.com>
2017-07-12 09:36:35 -0400

    Making this easier on myself

--- src/jasonwynn10/FakeAdmin/AdminEntity.php
+++ src/jasonwynn10/FakeAdmin/AdminEntity.php
@@ -7,6 +7,5 @@ use specter\api\DummyPlayer;
 class AdminEntity extends DummyPlayer {
     public function __construct($name, $address = null, $port = null, Server $server = null){
         parent::__construct($name, $address, $port, $server);
-        //
     }
 }
--- src/jasonwynn10/FakeAdmin/Main.php
+++ src/jasonwynn10/FakeAdmin/Main.php
@@ -1,12 +1,9 @@
 <?php
 namespace jasonwynn10\FakeAdmin;
 
-use pocketmine\entity\Effect;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
-use specter\network\SpecterPlayer;
-use specter\Specter;
+
 use spoondetector\SpoonDetector;
 
 class Main extends PluginBase implements Listener {
@@ -14,24 +11,15 @@ class Main extends PluginBase implements Listener {
 	private $authPlugin;
 	/** @var string $loginPassword */
 	private $loginPassword = "";
-	/** @var Specter $specter */
+	/** @var AdminEntity $specter */
 	private $specter;
 	public function onEnable() {
 		$this->saveDefaultConfig();
 		SpoonDetector::printSpoon($this,"spoon.txt");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->specter = $this->getServer()->getPluginManager()->getPlugin("Specter");
 		$this->authPlugin = $this->getServer()->getPluginManager()->getPlugin($this->getConfig()->getNested("Admin properties.authentication.plugin","")) ?? false;
 		if($this->authPlugin !== false)
 			$this->loginPassword = $this->getConfig()->getNested("Admin properties.authentication.password","");
-
-	}
-	public function onJoin(PlayerJoinEvent $ev) {
-		/** @var SpecterPlayer $player */
-		if(($player = $ev->getPlayer()) instanceof SpecterPlayer) {
-			if($this->getConfig()->getNested("Admin properties.invisible-to-non-op", false) == true) {
-				$player->addEffect(Effect::getEffect(Effect::INVISIBILITY)->setVisible(false)->setDuration(999));
-			}
-		}
+		$this->specter = new AdminEntity($this->getConfig()->getNested("Admin properties.Name","FakeAdmin"), $this->getConfig()->getNested("Admin properties.Ip","SPECTER"), (int)$this->getConfig()->getNested("Admin properties.Port",19133));
 	}
 }
